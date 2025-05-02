dcfcf052b447ba0d8af1e287127e3c858e5fc0f3
alabbar <abdullahalabbar920@gmail.com>
2025-05-02 22:06:09 +0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,21 @@
-mod lexer;
-mod parser;
-mod vm;
-mod error;
+use std::env;
+use std::fs;
+use c4_rust_Lahbab::[Lexer, Parser, Error, Result];
 
-fn main() {
-    println!("Welcome to the C4 Rust Compiler!");
+fn main() -> Result<()> {
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("usage: {} <source.c>", args[0]);
+        std::process::exit(1);
+    }
+
+    let src = fs::read_to_string(&args[1])
+        .map_err(|e| Error::InvalidSyntax(format!("I/O error: {}", e)))?;
+
+    let tokens = Lexer::new(&src).tokenize();
+    let mut parser = Parser::new(tokens);
+    let ast = parser.parse_program()?;
+
+    println!("{:#?}", ast);
+    Ok(())
 }
